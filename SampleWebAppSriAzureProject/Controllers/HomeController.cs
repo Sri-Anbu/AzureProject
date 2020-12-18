0483773326416bba965ba6b699e5a68a48982d6a
sridharshini <Sri.Anbu@lntinfotech.com>
2020-12-18 10:00:44 +0530

    AzureDemo

---
+++ SampleWebAppSriAzureProject/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleWebAppSriAzureProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "SriAnbu page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ SampleWebAppSriAzureProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SampleWebAppSriAzureProject.MvcApplication" Language="C#" %>
